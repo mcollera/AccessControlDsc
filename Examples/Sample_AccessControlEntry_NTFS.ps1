6c17ccf060de8ec0b1bac2ca0a8d9db882c9daf1
Reggie Gibson <31147354+regedit32@users.noreply.github.com>
2017-08-30 12:38:38 -0400

    Reggieg#ntfsaccess (#3)
    * Added NtfsAccess resource
    
    * Added verbose output; removed throws
    
    * small fixes for AppVeypr build
    
    * fixed mof encoding

---
+++ Examples/Sample_AccessControlEntry_NTFS.ps1
@@ -0,0 +1,76 @@
+﻿configuration Sample_NTFSAccessControl
+{
+    Import-DscResource -ModuleName AccessControlDsc
+    node $TargetName
+    {
+        NTFSAccessEntry Test
+        {
+            Path = "c:\test"
+            AccessControlList = @(
+                NTFSAccessControlList
+                {
+                    Principal = "Everyone"
+                    ForcePrincipal = $true
+                    AccessControlEntry = @(
+                        NTFSAccessControlEntry
+                        {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'FullControl'
+                            Inheritance = 'This folder and files'
+                            Ensure = 'Present'
+                        }
+                    )               
+                }
+                NTFSAccessControlList
+                {
+                    Principal = "Users"
+                    ForcePrincipal = $false
+                    AccessControlEntry = @(
+                        NTFSAccessControlEntry
+                        {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'FullControl'
+                            Inheritance = 'This folder and files'
+                            Ensure = 'Present'
+                        }
+                    )               
+                }
+            )
+        }
+        NTFSAccessEntry Test2
+        {
+            Path = "c:\test2"
+            AccessControlList = @(
+                NTFSAccessControlList
+                {
+                    Principal = "Everyone"
+                    ForcePrincipal = $true
+                    AccessControlEntry = @(
+                        NTFSAccessControlEntry
+                        {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'FullControl'
+                            Inheritance = 'This folder and files'
+                            Ensure = 'Present'
+                        }
+                    )               
+                }
+                NTFSAccessControlList
+                {
+                    Principal = "Users"
+                    ForcePrincipal = $false
+                    AccessControlEntry = @(
+                        NTFSAccessControlEntry
+                        {
+                            AccessControlType = 'Allow'
+                            FileSystemRights = 'FullControl'
+                            Inheritance = 'This folder and files'
+                            Ensure = 'Present'
+                        }
+                    )               
+                }
+            )
+        }
+    }
+}
+
