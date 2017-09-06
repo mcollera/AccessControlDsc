c8b051cefe9e72d304fdc03f79632d64889c98be
Reggie Gibson <reggieg@microsoft.com>
2017-09-05 21:08:20 -0400

    Removed dead files

--- Tests/Integration/AccessControlResourceHelper.integration.tests.ps1
+++
@@ -1,9 +0,0 @@
-#requires -RunAsAdministrator
-
-# Get the root path of the resourse
-[String] $script:moduleRoot = Split-Path -Parent ( Split-Path -Parent $PSScriptRoot )
-
-Import-Module -Name (Join-Path -Path $moduleRoot `
-                               -ChildPath 'DSCResources\AccessControlResourceHelper\AccessControlResourceHelper.psm1' ) `
-                               -Force
-#region Generate data
